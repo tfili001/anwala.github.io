50dd3e03cb3f0f73d1d60db990d6f53f5376d705
Tim <tfili001@odu.edu>
2018-02-13 14:21:40 -0500

    Delete parsejson.py

--- parsejson.py
+++
@@ -1,9 +0,0 @@
-import glob
-import json
-
-
-def parsejson():
-    for filename in glob.glob('/home/tim/Documents/anwala.github.io-master/Ignorethis_A2/p2JSON/*.json'):
-        print("fname:",filename)
-
-parsejson()
