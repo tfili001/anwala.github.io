9e25f30ff2fb0f2cbfed1af614493defbacdd63b
Tim <tfili001@odu.edu>
2018-02-19 17:34:14 -0500

    Add files via upload

--- parsejson.py
+++ parsejson.py
@@ -5,13 +5,17 @@ from glob import glob
 def parsejson():
     data = []
     i=0
-    path = "/home/tim/Documents/anwala.github.io-master/Ignorethis_A2/p2JSON"
+    path = "/home/tim/Documents/CS432/anwala.github.io-master/Ignorethis_A2/p2JSON"
     pattern = os.path.join(path, '*.json')
     for file_name in glob(pattern):
-        i=i+1
+        print(i," ",file_name)
         with open(file_name) as f:
-            data.append(json.load(f))
-            print(i," ",file_name)
+            try:
+                print(i)
+                data = json.load(f)
+                i=i+1
+            except:
+                print("File Invalid")
+                
 
 parsejson()
-
