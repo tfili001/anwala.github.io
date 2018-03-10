fc9dc0ab398c02d1ddecedf54bb5d3e650c96f53
Tim <tfili001@odu.edu>
2018-03-09 19:58:52 -0500

    Add files via upload

--- A5/A5.py
+++ A5/A5.py
@@ -1,27 +1,64 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 import networkx as centrality
+import math
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 
+def initialLabel():
+    labels = {}
+    labels[0]="0"
+    labels[1] = r'$John H$'
+    labels[33]="Mr. Hi"
+
+    for i in range(2,33):
+        labels[i]=str(i)
+
+    return labels
+
+
+def display(num):
+        pos = nx.spring_layout(G)
+        labels = initialLabel()
+        last=num
+        num=nx.number_connected_components(G)
+        colormap = ["red","lightgreen","orange","magenta","blue","yellow","purple"]
+        if nx.is_connected(G)==False:
+             res = []
+             for node in G.nodes():
+                 res.append(node)
+             comp = G.subgraph(res)
+             nx.draw_networkx(comp, pos=pos)
+
+             othersub = G.subgraph([0,1,3,4,5,6,7,10,11,12,13,16,17,19,21])
+             nx.draw_networkx(othersub,pos,with_labels=True,node_color=colormap[num-1],font_color='black',labels=labels)
+               
+        else:
+            nx.draw_networkx(G,pos,with_labels=True,node_color=colormap[num-1],font_color='black',labels=labels)
+
+        plt.show()
 
 def GirvanNewman(G):
-    ct=0
+    num=1
+    last=ct=0
     while([G.number_of_edges()==0]): 
-        ct+=1        
-        print(ct)
+
         allEdgesBetweenness = nx.edge_betweenness_centrality(G)
         b = allEdgesBetweenness.items()
         list = sorted(b,key=lambda x:x[1],reverse=True)
-        pos = nx.spring_layout(G)
-        nx.draw_networkx(G,pos)
-        #print("All neighbors",nx.clustering(G))
-        plt.show()
+        ct+=1
+        print(ct)
+        display(num)
         G.remove_edge(*list[0][0])
 
-x = 0
+
 G = nx.karate_club_graph()
 print("Node Degree")
+x=0
 for v in G:
     print('%s %s' % (v, G.degree(v)))
     x+=G.degree(v)
 
 GirvanNewman(G)
+
+
